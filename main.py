94c5e56234d3da55ca62c1bcecb7a12174035dfe
arad4228 <arad4228@gmail.com>
2023-04-06 22:55:07 +0900

    [KyoungJae[Split Function to class & Add main

--- main.py
+++ main.py
@@ -1,50 +1,15 @@
-import sys
-import os
 import PyQt5
-from PyQt5 import uic
-from PyQt5.QtWidgets import *
+import os, sys
+from mainWindow import *
 
+# QPA 관련 환경 설정을 위한 필수 명령어
 dirname = os.path.dirname(PyQt5.__file__)
 plugin_path = os.path.join(dirname, 'Qt5','plugins','platforms')
 os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
 
-main_form_class = uic.loadUiType("./uis/main_GUI.ui")[0]
-
-class WindowClass(QMainWindow, main_form_class):
-    nodeCount = 0
-    sink_status = 0 # 0 is auto, 1 is min settings
-
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.setWindowTitle("PET HashGraph")
-
-        # self.nodeNumber.valueChanged.connect(self.printValue)
-        self.pushButton.clicked.connect(self.click_Start)
-
-        self.group_checkBoxes.buttonClicked[int].connect(self.clicked_BtnSinkType)
-        # Auto Sink는 항상 켜진 상태이다.
-        self.checkBox_auto.toggle()
-
-    # def printValue(self):
-    #     print(self.nodeNumber.value())
-
-    # 다음의 화면으로 넘어간다.
-    def click_Start(self):
-        self.nodeCount = self.nodeNumber.value()
-
-    def clicked_BtnSinkType(self, id):
-        if self.checkBox_auto.isChecked():
-            print("Auto Clicked")
-            self.sink_status = 0
-        else:
-            print("Min Clicked")
-            self.sink_status = 1
-        print(self.sink_status)
-    
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    myWindow = WindowClass()
+    myWindow = Main_WindowClass()
     myWindow.show()
     app.exec_()
---
+++ mainWindow.py
@@ -0,0 +1,31 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import *
+
+main_form_class = uic.loadUiType("./uis/main_GUI.ui")[0]
+
+class Main_WindowClass(QMainWindow, main_form_class):
+    nodeCount = 0
+    sink_status = 0 # 0 is auto, 1 is min settings
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle("PET HashGraph")
+
+        # self.nodeNumber.valueChanged.connect(self.printValue)
+        self.pushButton.clicked.connect(self.click_Start)
+
+        self.group_checkBoxes.buttonClicked[int].connect(self.clicked_BtnSinkType)
+        # Auto Sink는 항상 켜진 상태이다.
+        self.checkBox_auto.toggle()
+
+
+    # 다음의 화면으로 넘어간다.
+    def click_Start(self):
+        self.nodeCount = self.nodeNumber.value()
+
+    def clicked_BtnSinkType(self, id):
+        if self.checkBox_auto.isChecked():
+            self.sink_status = 0
+        else:
+            self.sink_status = 1
