8b1a0296c553a280f5658e62e4d22a58689c9af7
arad4228 <arad4228@gmail.com>
2023-04-06 21:32:47 +0900

    [KyoungJae]Add CheckBox Btn actions

--- main.py
+++ main.py
@@ -11,20 +11,41 @@ os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
 main_form_class = uic.loadUiType("./uis/main_GUI.ui")[0]
 
 class WindowClass(QMainWindow, main_form_class):
+    nodeCount = 0
+    sink_status = 0 # 0 is auto, 1 is min settings
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
-        self.nodeNumber.valueChanged.connect(self.printValue)
+        # self.nodeNumber.valueChanged.connect(self.printValue)
         self.pushButton.clicked.connect(self.click_Start)
 
-    def printValue(self):
-        print(self.nodeNumber.value())
+        # Auto Sink는 항상 켜져있다.
+        self.checkBox_auto.toggle()
+        # 만약 min 버튼이 눌리거나 auto 버튼이 눌린다면
+        self.checkBox_min.clicked.connect(self.clicked_Btn_min)
+        self.checkBox_auto.clicked.connect(self.clicked_Btn_auto)
 
+    # def printValue(self):
+    #     print(self.nodeNumber.value())
+
+    # 다음의 화면으로 넘어간다.
     def click_Start(self):
-        self.printValue()
-        print("Start가 눌림")
+        self.nodeCount = self.nodeNumber.value()
 
+    # 만약 min 버튼이 눌렸다면
+    def clicked_Btn_min(self):
+        print("Min 버튼이 눌림")
+        self.checkBox_auto.toggle()
+        self.sink_status = 1
+    
+    # 만약 auto 버튼이 눌렸다면
+    def clicked_Btn_auto(self):
+        print("auto 버튼이 눌림")
+        self.checkBox_min.toggle()
+        self.sink_status = 0
+    
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
